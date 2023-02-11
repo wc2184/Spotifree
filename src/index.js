2d6eb868a563c9bb4774c758c0243a8f7a22301f
William Chan <wc2184@nyu.edu>
2023-02-11 05:06:58 -0500

    .

--- src/index.js
+++ src/index.js
@@ -50,10 +50,10 @@ function Root() {
 }
 
 const renderApplication = () => {
-  if (localStorage.getItem("version") != 1.6) {
+  if (localStorage.getItem("version") != 1.7) {
     // RESET LOCAL STORAGE
     localStorage.clear();
-    localStorage.setItem("version", 1.6);
+    localStorage.setItem("version", 1.7);
     localStorage.setItem("repeat", 1);
     localStorage.setItem("shuffle", false);
     localStorage.setItem("likes", JSON.stringify([]));
